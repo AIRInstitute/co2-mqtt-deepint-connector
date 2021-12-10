0709668c969397c3a95bcd7eb41a14711de0625e
Francisco Pinto-Santos <franpintosantos@usal.es>
2021-12-10 13:45:43 +0100

    .

---
+++ launch.sh
@@ -0,0 +1,18 @@
+#!usr/bin/python
+
+# Copyright 2021 AIR Institute
+# See LICENSE for details.
+
+
+MQTT_BROKER="212.128.140.31"
+MQTT_PORT="2780"
+MQTT_USER="bisite"
+MQTT_PASSWORD="bisite00"
+CONFIG_URL="http://212.128.140.31:8080/api/v1/co2/devices/info"
+DEEPINT_AUTH_TOKEN="JKM1wuXItbZ0JRF8ZdaNE6PW9iGmND2_Kyq2KVnG1MIPch_czSLXC1N24GLWjAZszI4eQrQuxRvdUxsNtF0KLw"
+MQTT_CLIENT_ID="example-id2"
+MQTT_NUM_MESSAGE_LIMIT="1"
+QUIET_MODE_SET="False"
+
+
+sudo co2-mqtt-deepint-connector "${MQTT_BROKER}" "${MQTT_PORT}" "${MQTT_USER}" "${MQTT_PASSWORD}" "${CONFIG_URL}" "${DEEPINT_AUTH_TOKEN}" "${MQTT_CLIENT_ID}" "${MQTT_NUM_MESSAGE_LIMIT}" "${QUIET_MODE_SET}"
