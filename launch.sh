598d0a5bf07b91f443715198b25ebe0dc289741f
Francisco Pinto Santos <franpintosantos@usal.es>
2021-12-13 10:18:35 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -3,7 +3,10 @@
 # Copyright 2021 AIR Institute
 # See LICENSE for details.
 
+# install newer version
+sudo python3 -m pip install . --upgrade
 
+# define parameters
 MQTT_BROKER="212.128.140.31"
 MQTT_PORT="2780"
 MQTT_USER="bisite"
@@ -14,5 +17,5 @@ MQTT_CLIENT_ID="example-id2"
 MQTT_NUM_MESSAGE_LIMIT="1"
 QUIET_MODE_SET="False"
 
-
+# run connector
 sudo co2-mqtt-deepint-connector "${MQTT_BROKER}" "${MQTT_PORT}" "${MQTT_USER}" "${MQTT_PASSWORD}" "${CONFIG_URL}" "${DEEPINT_AUTH_TOKEN}" "${MQTT_CLIENT_ID}" "${MQTT_NUM_MESSAGE_LIMIT}" "${QUIET_MODE_SET}"
